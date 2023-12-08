11ef612b0616142813918412367c448f12e7594c
BALA-HARI-S <balahari754@gmail.com>
2023-12-08 18:42:46 +0530

    Section operations2

--- course-management-system/src/Main.java
+++ course-management-system/src/Main.java
@@ -8,14 +8,15 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    private static CourseManager manager = new CourseManager();
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         // load sample data
-        CourseManager manager = new CourseManager();
         manager.loadSampleDataOne();
         manager.loadSampleDataTwo();
 
-        Scanner scanner = new Scanner(System.in);
 
         boolean flag = true;
         while (flag) {
@@ -29,7 +30,7 @@ public class Main {
                 case 0 -> flag = false;
                 case 1 -> {
                     System.out.println("Course Operation : writing course to file");
-                    manager.getListOfCourses().forEach(c -> System.out.printf("\t%d %s%n", c.getID(), c.getTitle()));
+                    manager.getListOfCourses().forEach(c -> System.out.printf("\tCourse %d %s%n", c.getID(), c.getTitle()));
                     System.out.print("\nChoose course to write : ");
                     int i = scanner.nextInt();
                     var course = manager.writeCourseToFile(manager.getListOfCourses().get(i - 1));
@@ -40,7 +41,7 @@ public class Main {
                     System.out.println("Course Operation : Read course from file");
                     int id = 1;
                     for (Path filePath : pathsOfFiles) {
-                        System.out.printf("\t%d %s%n", id, filePath.getFileName());
+                        System.out.printf("\tCourse %d %s%n", id, filePath.getFileName());
                         id++;
                     }
                     scanner.nextLine();
@@ -79,7 +80,7 @@ public class Main {
                 }
                 case 4 -> {
                     System.out.println("Course Operation : Remove course from list");
-                    manager.getListOfCourses().forEach(c -> System.out.printf("\t%d %s%n", c.getID(), c.getTitle()));
+                    manager.getListOfCourses().forEach(c -> System.out.printf("\tCourse %d %s%n", c.getID(), c.getTitle()));
                     System.out.print("\nChoose course to remove from list : ");
                     int i = scanner.nextInt();
                     manager.removeCourseFromList(i - 1);
@@ -88,7 +89,7 @@ public class Main {
                     System.out.println("Course Operation : Remove course file");
                     int id = 1;
                     for (Path filePath : pathsOfFiles) {
-                        System.out.printf("\t%d %s%n", id, filePath.getFileName());
+                        System.out.printf("\tCourse %d %s%n", id, filePath.getFileName());
                         id++;
                     }
                     System.out.println("Which course file do you want delete?(No) : ");
@@ -104,26 +105,27 @@ public class Main {
                 }
                 case 6 -> {
                     System.out.println("Course Operation : Edit course name");
-                    manager.getListOfCourses().forEach(c -> System.out.printf("\t%d %s%n", c.getID(), c.getTitle()));
+                    manager.getListOfCourses().forEach(c -> System.out.printf("\tCourse %d %s%n", c.getID(), c.getTitle()));
                     System.out.print("\nChoose course from list to change it's name : ");
                     int i = scanner.nextInt();
                     scanner.nextLine();
                     System.out.println("Enter new name for the course : ");
                     String title = scanner.nextLine();
-                    manager.getListOfCourses().get(i - 1).setTitle(title);
+                    manager.editCourseName(i, title);
+                    System.out.println("Course name successfully changed to " + title);
                 }
                 case 7 -> {
                     System.out.println("Course Operation : List available courses");
                     int courseId = 1;
                     for (Course course : manager.getListOfCourses()) {
-                        System.out.printf("\t%d %s%n", courseId, course.getTitle());
+                        System.out.printf("\tCourse %d %s%n", courseId, course.getTitle());
                         courseId++;
                     }
                 }
                 case 8 -> {
                     System.out.println("Course Operation : Total number of courses");
                     System.out.printf("Number of Courses in List : %d%nNumber of Course Files: %d",
-                            manager.getListOfCourses().size(), manager.getListOfCourseFiles(path1).size());
+                            manager.getCourseCount(), manager.getCourseFilesCount());
                 }
                 case 9 -> {
                     System.out.println("Section Operation : Add new section to course");
@@ -132,36 +134,69 @@ public class Main {
                     scanner.nextLine();
                     System.out.println("Section Title : ");
                     String sectionTitle = scanner.nextLine();
-
-                    int courseId = 1;
-                    for (Course course : manager.getListOfCourses()) {
-                        System.out.printf("\t%d %s%n", courseId, course.getTitle());
-                        courseId++;
-                    }
-
-                    System.out.print("\nChoose course from list to add section : ");
-                    int i = scanner.nextInt();
+                    int i = getCourseID("Choose course from list to add section : ");
                     scanner.nextLine();
                     manager.addSection(i, sectionID, sectionTitle);
                 }
-                case 13 -> {
-                    System.out.println("Section Operation : List available sections");
-                    int courseId = 1;
-                    for (Course course : manager.getListOfCourses()) {
-                        System.out.printf("\t%d %s%n", courseId, course.getTitle());
-                        courseId++;
+                case 10 -> {
+                    System.out.println("Section Operation : Remove section from course");
+                    int courseID = getCourseID("From which course do you want to remove section? Course(No.) : ");
+                    System.out.println("Sections available in " + manager.getListOfCourses().get(courseID - 1).getTitle());
+                    int sectionId = 1;
+                    for (Section section : manager.getListOfSections(courseID)) {
+                        System.out.printf("\tSection %d %s%n", sectionId, section.getTitle());
+                        sectionId++;
                     }
-                    System.out.print("\nChoose course(No.) from list : ");
-                    int courseNo = scanner.nextInt();
-                    scanner.nextLine();
+                    System.out.println("Select section(No.) to remove : ");
+                    int sectionID = scanner.nextInt();
+                    manager.removeSection(courseID, sectionID);
+                    System.out.println("Section successfully removed from course. Available sections");
 
+                    for (Section section : manager.getListOfSections(courseID)) {
+                        System.out.printf("\tSection %d %s%n", section.getID(), section.getTitle());
+                    }
+                }
+                case 11 -> {
+                    System.out.println("Section Operation : Edit section name");
+                    int courseID = getCourseID("From which course do you want to rename section? Course(No.) : ");
+                    System.out.println("Sections available in " + manager.getListOfCourses().get(courseID - 1).getTitle());
+                    int sectionId = 1;
+                    for (Section section : manager.getListOfSections(courseID)) {
+                        System.out.printf("\tSection %d %s%n", sectionId, section.getTitle());
+                        sectionId++;
+                    }
+                    System.out.println("Select section(No.) to rename : ");
+                    int sectionID = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Enter new section name : ");
+                    String title = scanner.nextLine();
+                    manager.editSectionName(courseID, sectionID, title);
+                    System.out.println("Course name successfully changed to " + title);
+
+                }
+                case 12 -> {
+                    System.out.println("Section Operation : List available sections");
+                    int courseNo = getCourseID("List sections from course(No.) : ");
+                    scanner.nextLine();
                     int sectionId = 1;
                     for (Section section : manager.getListOfSections(courseNo)) {
-                        System.out.printf("\t%d %s%n", sectionId, section.getTitle());
+                        System.out.printf("\tSection %d %s%n", sectionId, section.getTitle());
                         sectionId++;
                     }
                 }
-                case 19 -> {
+                case 13 -> {
+                    System.out.println("Section Operation : Total number of sections in a course");
+                    int courseID = getCourseID("From which course do you want to count sections? Course(No.) : ");
+                    System.out.printf("Total number of sections in %s is : %d",
+                            manager.getListOfCourses().get(courseID - 1).getTitle(), manager.getSectionsCount(courseID));
+                }
+                case 17 -> {
+                    System.out.println("Section operation : Sections with most lessons");
+                    var section = manager.getSectionWithMostLessons(getCourseID("From which course do you want to know section with most lessons ? Course(No.) : "));
+                    System.out.printf("'Section %d - %s' is the section with most lessons of count : %d%n",
+                            section.getID(), section.getTitle(), section.getLessons().size());
+                }
+                case 18 -> {
                     System.out.println("Lesson Operation : Add new lesson");
                     System.out.println("Lesson ID : ");
                     int lessonID = scanner.nextInt();
@@ -171,22 +206,16 @@ public class Main {
                     System.out.println("Lesson duration : ");
                     int duration = scanner.nextInt();
                     scanner.nextLine();
-                    System.out.print("\nChoose course and section to add lesson");
-                    int courseId = 1;
-                    for (Course course : manager.getListOfCourses()) {
-                        System.out.printf("\t%d %s%n", courseId, course.getTitle());
-                        courseId++;
-                    }
-                    System.out.print("\nChoose course(No.) from list : ");
-                    int courseNo = scanner.nextInt();
+                    System.out.println("Choose course and section to add a lesson");
+                    int courseNo = getCourseID("Choose course(No.) to add a lesson : ");
                     scanner.nextLine();
 
                     int sectionId = 1;
                     for (Section section : manager.getListOfSections(courseNo)) {
-                        System.out.printf("\t%d %s%n", sectionId, section.getTitle());
+                        System.out.printf("\tSection %d %s%n", sectionId, section.getTitle());
                         sectionId++;
                     }
-                    System.out.print("\nChoose section(No.) from list : ");
+                    System.out.print("\nChoose section(No.) to add a lesson : ");
                     int sectionNo = scanner.nextInt();
                     scanner.nextLine();
                     manager.addLesson(courseNo, sectionNo, lessonID, sectionTitle, duration);
@@ -200,7 +229,7 @@ public class Main {
 
     private static void printActions() {
         String textBlock = """
-                                
+                 
                 COURSE OPERATIONS
                 1) Write course to file
                 2) Read course from file
@@ -215,23 +244,33 @@ public class Main {
                 9) Add Section
                 10) Remove Section
                 11) Edit Section name
-                13) List Sections
-                14) Total number of sections
-                15) Shortest section based on duration
-                16) Longest section based on duration
-                17) Longest lesson in each section
-                18) Section with most lessons
+                12) List Sections
+                13) Total number of sections
+                14) Shortest section based on its Lesson's duration
+                15) Longest section based on its Lesson's duration
+                16) Longest lesson in each section based on its Lesson's duration
+                17) Section with most lessons
                                 
                 LESSON OPERATIONS
-                19) Add lesson
-                20) Remove lesson
-                21) Edit lesson name
-                22) List lessons
-                23) Lessons with same keyword
-                24) Total number of lessons
+                18) Add lesson
+                19) Remove lesson
+                20) Edit lesson name
+                21) List lessons
+                22) Lessons with same keyword
+                23) Total number of lessons
                 Press 0 to exit()
                 """;
         System.out.println(textBlock);
     }
 
+    private static int getCourseID(String message) {
+        int id = 1;
+        for (Course course : manager.getListOfCourses()) {
+            System.out.printf("\tCourse %d %s%n", id, course.getTitle());
+            id++;
+        }
+        System.out.print("\nFrom which course do you want to know section with most lessons ? Course(No.) : ");
+        int courseID = scanner.nextInt();
+        return courseID;
+    }
 }
--- course-management-system/src/services/CourseManager.java
+++ course-management-system/src/services/CourseManager.java
@@ -6,7 +6,9 @@ import entities.Section;
 import utilities.CourseFileHandler;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -35,19 +37,19 @@ public class CourseManager {
     public void loadSampleDataTwo() {
         var webDevelopment = createNewCourse(2, "Web Development for front-end developers", "Tim", "29/12/23",
                 200.00, 4.5, "y");
-
         var section1 = addSection(webDevelopment.getID(), 1, "Course Introduction");
         addLesson(webDevelopment.getID(), section1.getID(), 1, "Introduction to the course", 2);
         addLesson(webDevelopment.getID(), section1.getID(), 2, "Remaster in progress", 10);
+        addLesson(webDevelopment.getID(), section1.getID(), 3, "Introduction to the HTML,CSS,JS", 2);
+        addLesson(webDevelopment.getID(), section1.getID(), 4, "Remaster in progress", 10);
         var section2 = addSection(webDevelopment.getID(), 2, "VS code Setup");
-        addLesson(webDevelopment.getID(), section2.getID(), 1, "VS code Installation", 2);
-        addLesson(webDevelopment.getID(), section2.getID(), 2, "Remaster in progress", 3);
-
+        addLesson(webDevelopment.getID(), section2.getID(), 5, "VS code Installation", 2);
+        addLesson(webDevelopment.getID(), section2.getID(), 6, "Remaster in progress", 3);
         writeCourseToFile(webDevelopment);
     }
 
     public Course writeCourseToFile(Course course) {
-        Path coursePath = fileHandler.writeCourse(course);
+        fileHandler.writeCourse(course);
         return course;
     }
 
@@ -88,8 +90,17 @@ public class CourseManager {
         return fileHandler.listCourseFiles(path);
     }
 
-    public List<Section> getListOfSections(int courseID) {
-        return getListOfCourses().get(courseID - 1).getSections();
+    public int getCourseCount() {
+        return getListOfCourses().size();
+    }
+
+    public int getCourseFilesCount() {
+        return getListOfCourseFiles(Paths.get(".")).size();
+    }
+
+    public void editCourseName(int courseID, String title) {
+        var course = getListOfCourses().get(courseID - 1);
+        course.setTitle(title);
     }
 
     public Section addSection(int courseID, int sectionID, String title) {
@@ -98,8 +109,28 @@ public class CourseManager {
         return section;
     }
 
-    public boolean removeSection(Path path) {
-        return fileHandler.removeCourseFile(path);
+    public void removeSection(int courseID, int sectionID) {
+        var sections = getListOfCourses().get(courseID - 1).getSections();
+        sections.remove(sectionID - 1);
+    }
+
+    public void editSectionName(int courseID, int sectionID, String title) {
+        var section = getListOfCourses().get(courseID - 1).getSections().get(sectionID - 1);
+        section.setTitle(title);
+    }
+
+    public List<Section> getListOfSections(int courseID) {
+        return getListOfCourses().get(courseID - 1).getSections();
+    }
+
+    public int getSectionsCount(int courseID) {
+        return getListOfCourses().get(courseID - 1).getSections().size();
+    }
+
+    public Section getSectionWithMostLessons(int courseID) {
+        var sections = getListOfSections(courseID);
+        sections.sort(new SectionLessonComparator().reversed());
+        return sections.get(0);
     }
 
     public Lesson addLesson(int courseID, int sectionID,
@@ -127,3 +158,11 @@ public class CourseManager {
 
 
 }
+
+class SectionLessonComparator implements Comparator<Section> {
+    @Override
+    public int compare(Section section1, Section section2) {
+        return Integer.compare(section1.getLessons().size(), section2.getLessons().size());
+    }
+}
+
